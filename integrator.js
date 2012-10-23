8c9f6cec56b9b3b893393d1cb84fa720804a1418
Bjørn Einar Bjartnes <bjartwolf@bjartwolf-desktop.(none)>
2012-10-23 22:08:52 +0200

    speedy stuff

--- integrator.js
+++ integrator.js
@@ -1,16 +1,30 @@
 var Rx = require('rx');
-var constant = Rx.Observable.interval(100).select(function () { return 1 ;});
+var periodInMs = 100;
+var deltaT = 1/periodInMs;
+var gravity = Rx.Observable.interval(periodInMs).select(function () { return 9.81 ;});
 
-var integrator  = (function() {
-    var acc = 0
-      , k = 0.04;
-    return constant.select( function (val) {
-        acc += val - (k * acc);
-        return acc;
+var accelerationIntegrator = (function(initialSpeed) {
+    var speed = initialSpeed,
+        airResistance = 0.03;
+    return gravity.select( function (acceleration) {
+        speed += deltaT*(acceleration - airResistance * speed * speed)  ;
+        return speed;
     });
-}());
+}(0)); // Initial value to function (initialSpeed)
 
-integrator.subscribe(function (val) {
-    console.log(val);
+var velocityIntegrator = (function(initialPosition) {
+    var position = initialPosition;
+    return accelerationIntegrator.select( function (velocity) {
+        position -= deltaT*velocity;
+        if (position < 0) {console.log('CRASH!');}
+        return position;
+    });
+}(1000)); // Initial value to function (initialPosition)
+
+accelerationIntegrator.subscribe(function (val) {
+    console.log('Speed: ' + val);
+});
+velocityIntegrator.subscribe(function (val) {
+    console.log('Position: ' + val);
 });
 
