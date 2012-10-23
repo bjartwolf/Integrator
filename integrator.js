52e48cedf9579f38b17bd13fc225aec683c7f0b6
Bjartwolf <bjartwolf@bjartwolf-Dell-System-XPS-L321X.(none)>
2012-10-23 09:31:00 +0200

    eureka

--- integrator.js
+++ integrator.js
@@ -1,11 +1,14 @@
 var Rx = require('rx');
 var constant = Rx.Observable.interval(100).select(function () { return 1 ;});
-acc = 0;
-k = 0.4;
-var integrator = constant.select( function (val) {
-    acc += val - (k * acc);
-    return acc;
-});
+
+var integrator  = (function() {
+    var acc = 0
+      , k = 0.04;
+    return constant.select( function (val) {
+        acc += val - (k * acc);
+        return acc;
+    });
+}());
 
 integrator.subscribe(function (val) {
     console.log(val);
